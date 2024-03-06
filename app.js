5f16084d9d5d04197a1ab7dca702d621ea754065
Moin Alam <moinalam010@gmail.com>
2024-03-06 20:57:22 +0600

    Complete

---
+++ app.js
@@ -0,0 +1,27 @@
+const button = document.querySelector("button");
+const popupBG = document.querySelector(".popup-wrapper");
+const popup = document.querySelector(".popup");
+const cross = document.querySelector(".cross-btn");
+
+
+button.addEventListener("click", ()=>{
+    popupBG.style.display = "block";
+})
+
+cross.addEventListener("click", ()=>{
+    popupBG.style.display = "none";
+})
+
+popup.addEventListener("click", ()=>{
+    
+})
+
+popupBG.addEventListener("click", (event)=>{
+    if(!event.target.classList.contains("popup")){
+        popupBG.style.display = "none";
+    }
+})
+
+
+
+
